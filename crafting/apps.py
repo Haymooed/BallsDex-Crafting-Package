6b8f3963846bc67f3ed0f15ee59ddda093cf3ed3
Haymooed <167770628+Haymooed@users.noreply.github.com>
2026-01-09 22:20:31 +1030

    Update apps.py

--- crafting/apps.py
+++ crafting/apps.py
@@ -1,9 +1,8 @@
 from django.apps import AppConfig
 
 
-class CraftingConfig(AppConfig):
-    name = "crafting"
-    label = "crafting"  # avoid conflicts
-    verbose_name = "BallsDex Crafting"
-    dpy_package = "crafting.package"
+class ReportConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
+    name = "craft"
+    verbose_name = "Crafting package"
+    dpy_package = "crafting.package"
